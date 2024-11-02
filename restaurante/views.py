86eb4f3ee3ec797ab4ee161798643d0fda5bd818
Ivan Nakatani <ivan.nakatani@gmail.com>
2024-11-02 04:28:01 -0300

    comentarios pra ajudar

--- restaurante/views.py
+++ restaurante/views.py
@@ -21,45 +21,68 @@ class CadastrarItem(APIView):
 
 class CadastrarMesa(APIView):
     def post(self, request):
+        #adicionar logica pra salvar mesa (algo similar ao de cima)
         return JsonResponse()
 
 class ListarMesa(APIView):
     def get(self, request):
         lista_mesas = list(Mesa.objects.all().values())
-
         return JsonResponse(data={'mesas': lista_mesas}, status=200)
 
 class CadastrarCliente(APIView):
     def post(self, request):
+        # adicionar logica pra salvar cliente (algo similar ao de item)
         return JsonResponse()
 
 class CriarPedido(APIView):
     def post(self, request):
+        # adicionar logica pra salvar pedido (algo similar ao de item)
         return JsonResponse()
 
 class ListarPedido(APIView):
     def get(self, request):
+        # listar pedidos exceto status cancelado (algo similar ao de listar mesas)
+        # mas adicionar a propriedade exclude
+        # exemplo: Pedido.objects.exclude(status__descricao='Cancelado')
         return JsonResponse()
 
 class AlterarPedido(APIView):
     def post(self, request):
+        # adicionar logica pra alterar pedido
+        # 1 pegar no banco o valor com o id q vai vir do request
+        # fazer alteracao do valor
+        # salvar no banco
         return JsonResponse()
 
 class AtualizarStatusPedido(APIView):
     def post(self, request):
+        # adicionar logica pra atualizar status pedido
+        # 1 pegar no banco o valor com o id q vai vir do request
+        # fazer alteracao do valor
+        # salvar no banco
         return JsonResponse()
 
 class CancelarPedido(APIView):
     def post(self, request):
+        # adicionar logica pra cancelar pedido
+        # 1 pegar no banco o valor com o id q vai vir do request
+        # fazer alteracao do valor
+        # salvar no banco
         return JsonResponse()
 
 class FecharPedido(APIView):
     def post(self, request):
+        # adicionar logica pra fechar pedido
+        # 1 pegar no banco o valor com o id q vai vir do request
+        # fazer alteracao do valor
+        # salvar no banco
         return JsonResponse()
 class CalcularValorPedido(APIView):
     def get(self, request):
+        #pegar o valor dos itens do pedido e somar
         return JsonResponse()
 
 class DividirValorPedido(APIView):
     def post(self, request):
+        # pegar o valor dos itens do pedido e somar e divir pelo numero de pessoas
         return JsonResponse()
